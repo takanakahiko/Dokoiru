18de923bb76cfd5aea21a6285493d846622008f8
takanakahiko <takanakahiko@gmail.com>
2023-07-10 01:11:15 +0900

    try to the display the map

--- Dokoiru/ContentView.swift
+++ Dokoiru/ContentView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State  var region = MKCoordinateRegion(
+        center : CLLocationCoordinate2D(
+            latitude: 35.710057714926265,  // 緯度
+            longitude: 139.81071829999996 // 経度
+        ),
+        latitudinalMeters: 1000.0, // 南北
+        longitudinalMeters: 1000.0 // 東西
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        // 地図を表示
+        Map(coordinateRegion: $region)
+            .edgesIgnoringSafeArea(.bottom)
     }
 }
 
