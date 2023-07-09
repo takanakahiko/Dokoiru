c57281e88a5e403da86f4e06ec7c1340394be4ee
takanakahiko <takanakahiko@gmail.com>
2023-07-09 23:10:23 +0900

    Initial Commit

---
+++ Dokoiru/DokoiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  DokoiruApp.swift
+//  Dokoiru
+//
+//  Created by takanakahiko on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct DokoiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
